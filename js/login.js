69b8925abaa44e4c16450dc51d54807ca85e29e1
turjorahman <turjorahman333@gmail.com>
2023-10-21 23:47:12 +0600

    site updated

--- js/login.js
+++ js/login.js
@@ -4,7 +4,7 @@ document.getElementById('submit-btn').addEventListener('click', function()
     const password = document.getElementById('user-password');
 
 
-    if(email.value == "turjorahman@gmail.com" && password.value == "87654321")
+    if(email.value == "sample@gmail.com" && password.value == "12345")
     {
         window.location.href = 'http://127.0.0.1:5500/bank.html'
     }
